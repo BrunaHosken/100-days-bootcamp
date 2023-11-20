b8bc2bcfb0acdc267a4ec80e7612667bfc1cff68
BrunaHosken <43359365+BrunaHosken@users.noreply.github.com>
2023-11-20 19:41:58 +0000

    finishing day 1

--- day1.py
+++ day1.py
@@ -1,3 +1,16 @@
 #day1
 #printing to the console
-print("Hello world!")
+print("Hello world!\nHello world!")
+print("Hello" + " " + "Bruna")
+
+name = input("What is your name?")
+length = len(name)
+print(name)
+print(length)
+
+print("----------------------------------------")
+print("Day 1 Project - Band Name Generator")
+print("\nWelcome to the Band Name Generator")
+city=input("What's name of the city you grew up in?\n")
+pet=input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
