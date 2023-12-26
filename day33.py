0e3c998809a15fbca20b7dc3f8070df19ead7119
BrunaHosken <bruna16.hosken@gmail.com>
2023-12-25 23:37:26 -0300

    correct logic

--- day33.py
+++ day33.py
@@ -12,43 +12,44 @@ password="gjlztlmeifwusepf"
 LAT = -22.289181
 LONG = -42.534561
 
+def iss_position():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
 
-response = requests.get(url = "http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
 
-data = response.json()
-longitude = float(data["iss_position"]["longitude"])
-latitude = float(data["iss_position"]["latitude"])
+    latitude = iss_latitude >= MY_LONG-5 and iss_latitude <= MY_LONG+5
+    longitude = iss_longitude >= MY_LONG-5 and iss_longitude <= MY_LAT+5
 
-parametres = {
-    "lat":LAT,
-    "lng":LONG,
-    "formatted": 0
-}
-response = requests.get("https://api.sunrise-sunset.org/json", params=parametres)
-response.raise_for_status()
-data = response.json()
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    return latitude and longitude
 
-time_now = datetime.now()
-time_hour = time_now.hour
+def is_dark():
+    parametres = {
+        "lat":MY_LAT,
+        "lng":MY_LONG,
+        "formatted": 0
+    }
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parametres)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
+    time_now = datetime.now()
+    time_hour = time_now.hour
 
-is_dark =  sunrise >= time_hour >= sunset
-current_location_lng = LONG - 5 <= longitude <= LONG + 5
-current_location_lat = LAT - 5 <= latitude <= LAT + 5
-
+    return time_hour >= sunset or time_hour <= sunrise
 
 while True:
-    time.sleep(20)
-    if(is_dark and current_location_lng and current_location_lat):
+    time.sleep(30)
+    if( iss_position() and is_dark()):
         with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
             connection.starttls()
             connection.login(user=my_email,password=password)
             connection.sendmail(from_addr=my_email, to_addrs="bruna@gmail.com", msg=f"Subject:ISS Notifier\n\n Look to the sky")
             connection.close()
-
      
 
  
