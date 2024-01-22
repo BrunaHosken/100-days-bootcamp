c35231f625887293f675c7f4a72044eb57e8387d
BrunaHosken <bruna16.hosken@gmail.com>
2024-01-22 00:47:17 -0300

    finishing day 60

--- day59/main.py
+++ day59/main.py
@@ -1,5 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
+from dotenv import load_dotenv
+import smtplib
 import requests
+
+load_dotenv()
+
+my_email = os.getenv('EMAIL_FROM')
+password=os.getenv('PASSWORD_FROM')
 
 posts = requests.get("https://api.npoint.io/c6f9af3cb7e76125a01a").json()
 
@@ -24,6 +32,25 @@ def get_post(index):
         if post["id"] == index:
             requested = post
     return render_template("post.html", post = requested)
+
+@app.route("/contact", methods=["GET","POST"])
+def receive_data():
+    if(request.method == "POST"):
+        data=request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        
+        return render_template("contact.html", msg_sent= True)
+    
+    return render_template("contact.html", msg_sent= False)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=my_email,password=password)
+            connection.sendmail(from_addr=my_email, to_addrs=my_email, msg=email_message)
+            connection.close()
 
 if __name__ == "__main__":
     app.run(debug=True)
