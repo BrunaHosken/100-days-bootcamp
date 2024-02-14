cc77e2e9dbc6acfe1ebd230d93e7805d2129fa6e
BrunaHosken <bruna16.hosken@gmail.com>
2024-02-14 16:36:21 -0300

    adjusting import

--- advanced/day62/main.py
+++ advanced/day62/main.py
@@ -59,7 +59,7 @@ def add_cafe():
 
 @app.route('/cafes')
 def cafes():
-    with open('./day62/cafe-data.csv', newline='', encoding='utf-8') as csv_file:
+    with open('./advanced/day62/cafe-data.csv', newline='', encoding='utf-8') as csv_file:
         csv_data = csv.reader(csv_file, delimiter=',')
         list_of_rows = []
         for row in csv_data:
